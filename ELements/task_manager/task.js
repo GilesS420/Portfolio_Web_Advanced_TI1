ffc7e872eb0ff7e547cd888591e2b61b86fa74b8
Giles Synaeve <giles.synaeve@student.ehb.be>
2024-06-05 23:11:36 +0200

    fix: mùain page css en overbodige elementen verwijdert

--- ELements/task_manager/task.js
+++ ELements/task_manager/task.js
@@ -1,7 +1,7 @@
 document.getElementById('showSummaryBtn').addEventListener('click', function() {
     generateSummary(); 
 });
-
+ updateHeader();
 function generateSummary() {
     const user = JSON.parse(localStorage.getItem('user'));
     const cardIds = Object.keys(localStorage).filter(key => key.startsWith('card_'));
@@ -14,7 +14,7 @@ function generateSummary() {
     }
 
     if (cardsData.length > 0) {
-        summary += '<h3>Kaarten:</h3><ul>';
+        summary += '<h3>Interesses :</h3><ul>';
         cardsData.forEach(cardData => {
             summary += `<li> ${cardData.text}, ${cardData.color}</li>`;
         });
@@ -40,9 +40,11 @@ document.addEventListener('DOMContentLoaded', () => {
 function updateHeader() {
     const userNameSpan = document.getElementById('userName');
     const userAgeSpan = document.getElementById('userAge');
-    const user = jsonData [0];
-    if (user) {
-        userNameSpan.textContent = user.name;
-        userAgeSpan.textContent = user.age;
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user){
+    userNameSpan.textContent = user.name;
+     userAgeSpan.textContent = user.age;
     }
+    
 }
